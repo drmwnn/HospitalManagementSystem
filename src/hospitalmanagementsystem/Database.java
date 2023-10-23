062e4dde7144536040428d29963f5aa50ca02dc6
drmwnn <asep.darmawan879@gmail.com>
2023-10-23 10:33:45 +0700

    first commit

---
+++ src/hospitalmanagementsystem/Database.java
@@ -0,0 +1,18 @@
+package hospitalmanagementsystem;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class Database {
+    public static Connection connectDB() {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection connect
+                    = DriverManager.getConnection("jdbc:mysql://localhost:3306/hospital", "root", "Java1234");
+            return connect;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
---
+++ src/hospitalmanagementsystem/Users.java
@@ -0,0 +1,5 @@
+package hospitalmanagementsystem;
+public class Users {
+    public static String[] user = {"Admin Portal", "Doctor Portal", "Patient Portal"};
+
+}
